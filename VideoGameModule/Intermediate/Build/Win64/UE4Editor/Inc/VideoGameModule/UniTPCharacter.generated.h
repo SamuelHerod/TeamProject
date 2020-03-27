41c3153c1f7de4a7469a6b9b76d5095028a57ad9
Jed Whitemore <49200495+Zephy-o@users.noreply.github.com>
2020-03-27 10:07:39 +0000

    Fixed Goblin Instancing

---
+++ VideoGameModule/Intermediate/Build/Win64/UE4Editor/Inc/VideoGameModule/UniTPCharacter.generated.h
@@ -0,0 +1,93 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef VIDEOGAMEMODULE_UniTPCharacter_generated_h
+#error "UniTPCharacter.generated.h already included, missing '#pragma once' in UniTPCharacter.h"
+#endif
+#define VIDEOGAMEMODULE_UniTPCharacter_generated_h
+
+#define VideoGameModule_Source_VideoGameModule_UniTPCharacter_h_12_SPARSE_DATA
+#define VideoGameModule_Source_VideoGameModule_UniTPCharacter_h_12_RPC_WRAPPERS
+#define VideoGameModule_Source_VideoGameModule_UniTPCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define VideoGameModule_Source_VideoGameModule_UniTPCharacter_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAUniTPCharacter(); \
+	friend struct Z_Construct_UClass_AUniTPCharacter_Statics; \
+public: \
+	DECLARE_CLASS(AUniTPCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/VideoGameModule"), NO_API) \
+	DECLARE_SERIALIZER(AUniTPCharacter)
+
+
+#define VideoGameModule_Source_VideoGameModule_UniTPCharacter_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAUniTPCharacter(); \
+	friend struct Z_Construct_UClass_AUniTPCharacter_Statics; \
+public: \
+	DECLARE_CLASS(AUniTPCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/VideoGameModule"), NO_API) \
+	DECLARE_SERIALIZER(AUniTPCharacter)
+
+
+#define VideoGameModule_Source_VideoGameModule_UniTPCharacter_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AUniTPCharacter(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AUniTPCharacter) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AUniTPCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AUniTPCharacter); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AUniTPCharacter(AUniTPCharacter&&); \
+	NO_API AUniTPCharacter(const AUniTPCharacter&); \
+public:
+
+
+#define VideoGameModule_Source_VideoGameModule_UniTPCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AUniTPCharacter(AUniTPCharacter&&); \
+	NO_API AUniTPCharacter(const AUniTPCharacter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AUniTPCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AUniTPCharacter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AUniTPCharacter)
+
+
+#define VideoGameModule_Source_VideoGameModule_UniTPCharacter_h_12_PRIVATE_PROPERTY_OFFSET
+#define VideoGameModule_Source_VideoGameModule_UniTPCharacter_h_9_PROLOG
+#define VideoGameModule_Source_VideoGameModule_UniTPCharacter_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	VideoGameModule_Source_VideoGameModule_UniTPCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	VideoGameModule_Source_VideoGameModule_UniTPCharacter_h_12_SPARSE_DATA \
+	VideoGameModule_Source_VideoGameModule_UniTPCharacter_h_12_RPC_WRAPPERS \
+	VideoGameModule_Source_VideoGameModule_UniTPCharacter_h_12_INCLASS \
+	VideoGameModule_Source_VideoGameModule_UniTPCharacter_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define VideoGameModule_Source_VideoGameModule_UniTPCharacter_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	VideoGameModule_Source_VideoGameModule_UniTPCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	VideoGameModule_Source_VideoGameModule_UniTPCharacter_h_12_SPARSE_DATA \
+	VideoGameModule_Source_VideoGameModule_UniTPCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	VideoGameModule_Source_VideoGameModule_UniTPCharacter_h_12_INCLASS_NO_PURE_DECLS \
+	VideoGameModule_Source_VideoGameModule_UniTPCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> VIDEOGAMEMODULE_API UClass* StaticClass<class AUniTPCharacter>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID VideoGameModule_Source_VideoGameModule_UniTPCharacter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ VideoGameModule/Intermediate/Build/Win64/VideoGameModuleEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "E:/Github/TeamProject/VideoGameModule/Intermediate/Build/Win64/VideoGameModuleEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
